4352a3c3d55dd82666c787c3dff6ca69da0fc416
Kevin Knights <kevinknights29@hotmail.com>
2024-03-24 20:16:40 +0000

    feat: added docstring, response storage, and output sorting based on absolute time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import json
 import os
 import re
+from collections import namedtuple
 from datetime import datetime
 from datetime import timedelta
 
@@ -18,8 +19,23 @@ email_address = os.environ.get("LINKEDIN_EMAIL_ADDRES")
 password = os.environ.get("LINKEDIN_PASSWORD")
 public_id = os.environ.get("LINKEDIN_PUBLIC_ID")
 
+# Create Data Structures
+Post = namedtuple("Post", ["poster_name", "post_content", "relative_timestamp", "absolute_timestamp"])
+
 
 def clean_relative_time(time_str: str) -> tuple[int, str]:
+    """Clean relative time string and extract quantity and unit
+
+    Args:
+        time_str (str): string containing relative time
+
+    Returns:
+        tuple[int, str]: cleaned quantity and unit
+
+    Example:
+        >>> clean_relative_time("1d ago")
+        (1, 'd')
+    """
     match = re.search(r"(\d+)([a-zA-Z])", time_str)
     if match:
         number = int(match.group(1))
@@ -30,6 +46,15 @@ def clean_relative_time(time_str: str) -> tuple[int, str]:
 
 
 def extract_and_convert_time(quantity: int, unit: str) -> datetime | None:
+    """Extract and convert relative time to absolute time
+
+    Args:
+        quantity (int): quantity of time
+        unit (str): unit of time
+
+    Returns:
+        datetime | None: absolute time
+    """
     # Mapping relative time units to timedelta arguments
     time_units = {
         "s": "seconds",
@@ -58,6 +83,12 @@ profile = api.get_profile(public_id=public_id)
 # Get profile posts
 posts = api.get_profile_posts(urn_id=profile["urn_id"], post_count=10)
 
+# Save extracted posts to a JSON file
+export_dir = os.path.join(os.getcwd(), "data")
+os.makedirs(export_dir, exist_ok=True)
+export_path = os.path.join(export_dir, "extracted_posts.json")
+with open(export_path, "w") as file:
+    json.dump(posts, file, indent=4)
 
 # Key to extract reposts: ["header"]["text"]["text"]
 # Key to extract poster name: ["actor"]["name"]["text"]
@@ -65,22 +96,31 @@ posts = api.get_profile_posts(urn_id=profile["urn_id"], post_count=10)
 # Key to extract relative timestamp ["actor"]["subDescription"]["text"]
 curated_posts = []
 for post in posts:
-    if "repost" in post["header"]["text"]["text"]:
-        info = {}
-        info["poster_name"] = post["actor"]["name"]["text"]
-        info["post_content"] = re.sub(r"[^\w\s]", "", post["commentary"]["text"]["text"]).strip()
+    condition = "header" in post and "text" in post.get("header") and "text" in post.get("header").get("text")
+    if condition:
+        poster_name = post["actor"]["name"]["text"]
+        post_content = re.sub(r"[^\w\s]", "", post["commentary"]["text"]["text"]).strip()
         quantity, unit = clean_relative_time(post["actor"]["subDescription"]["text"])
-        info["relative_timestamp"] = f"{quantity}{unit} ago"
+        relative_timestamp = f"{quantity}{unit} ago"
         absolute_timestamp = extract_and_convert_time(quantity, unit)
-        if absolute_timestamp:
-            info["absolute_timestamp"] = absolute_timestamp.isoformat()
-        curated_posts.append(info)
-    else:
-        continue
+        try:
+            curated_posts.append(Post(poster_name, post_content, relative_timestamp, absolute_timestamp))
+        except Exception as e:
+            print("Something went wrong with Post creation...", e, sep=" ")
+sorted_posts = sorted(curated_posts, key=lambda post: post.absolute_timestamp)
+sorted_posts_dicts = [
+    {
+        "poster_name": post.poster_name,
+        "post_content": post.post_content,
+        "relative_timestamp": post.relative_timestamp,
+        "absolute_timestamp": post.absolute_timestamp.isoformat(),  # Convert datetime to ISO format string
+    }
+    for post in sorted_posts
+]
 
-# Save posts to a JSON file
+# Save curated posts to a JSON file
 export_dir = os.path.join(os.getcwd(), "data")
 os.makedirs(export_dir, exist_ok=True)
 export_path = os.path.join(export_dir, "reposted_posts.json")
 with open(export_path, "w") as file:
-    json.dump(curated_posts, file, indent=4)
+    json.dump(sorted_posts_dicts, file, indent=4)
